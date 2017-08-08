ebb4122445bbbfd892aff4c4b3947420e1d4b8f1
ovv <noking007@mail.ru>
2017-08-09 02:24:47 +0300

    Add NetworkConnector implementation

--- routing/include/NetworkConnector/NetworkConnector.h
+++ routing/include/NetworkConnector/NetworkConnector.h
@@ -4,6 +4,8 @@
 #pragma once
 
 #include "boost/asio.hpp"
+#include "boost/asio/ip/udp.hpp"
+
 #include <vector>
 #include <string>
 #include <functional>
@@ -11,24 +13,33 @@
 
 namespace m2 {
 namespace routing {
+
+class Node;
+
 using std::string;
 using std::vector;
+using namespace boost::asio::ip;
+using boost::asio::io_service;
 
-class NetworkConnector
+class NetworkConnector final
 {
  public:
-  NetworkConnector(int port, std::function<void(char* buffer, size_t size)> onMessageReceiveCallback);
+  NetworkConnector(int port,
+                   io_service& service,
+                   std::function<void(vector<char>)> on_message_callback);
 
-  NetworkConnector();
+
+  NetworkConnector() = default;
+  NetworkConnector(NetworkConnector&) = delete;
 
   void sendMessage(string ip, int port, vector<char> buffer);
   void startAccept();
   string getMyIpAddress();
 
  private:
-  std::function<void(char* buffer, size_t size)> callback;
-//    asio::socket
-//    std::array<char, const_size> buffer;
+  std::function<void(vector<char> buffer)> on_message_callback;
+  udp::socket socket;
+  std::array<char, 2000> buffer;
 };
 
 } // end routing
