80345dbc08ed8b438023d6928f81676fd4717ba3
ovv <noking007@mail.ru>
2017-08-09 20:49:08 +0300

    Change 2 indents to 4

--- routing/include/NetworkConnector/NetworkConnector.h
+++ routing/include/NetworkConnector/NetworkConnector.h
@@ -24,22 +24,22 @@ using boost::asio::io_service;
 class NetworkConnector final
 {
  public:
-  NetworkConnector(int port,
-                   io_service& service,
-                   std::function<void(vector<char>)> on_message_callback);
+    NetworkConnector(int port,
+                     io_service& service,
+                     std::function<void(vector<char>)> on_message_callback);
 
 
-  NetworkConnector() = default;
-  NetworkConnector(NetworkConnector&) = delete;
+    NetworkConnector() = default;
+    NetworkConnector(NetworkConnector&) = delete;
 
-  void sendMessage(string ip, int port, vector<char> buffer);
-  void startAccept();
-  string getMyIpAddress();
+    void sendMessage(string ip, int port, vector<char> buffer);
+    void startAccept();
+    string getMyIpAddress();
 
  private:
-  std::function<void(vector<char> buffer)> on_message_callback;
-  udp::socket socket;
-  std::array<char, 2000> buffer;
+    std::function<void(vector<char> buffer)> on_message_callback;
+    udp::socket socket;
+    std::array<char, 2000> buffer;
 };
 
 } // end routing
