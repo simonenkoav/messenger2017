31eda52a0510bb0f428bb84310376bc0cd810b14
dkutre <noking007@mail.ru>
2017-08-09 22:06:16 +0300

    Add DHT class and fix boost::system error in includes (#16)
    * Add build.sh scrypt
    
    * Add NetworkConnector declaration
    
    * Add DHT declaration
    
    * Add getMyIpAddress in NetworkConnector
    
    * Add MessageType enumeration and hash-fucntion(messagyType)
    
    * Add Node declaration and part of implementation
    
    * change camelStyle to under_score and add Node(string)
    
    * Add Node::Node() implementation
    
    * Fix errors in Node.h, Node.cpp
    
    * Add NetworkConnector implementation
    
    * Fix initialise problems in Node(string)
    
    * Add DHT declaration and implementation
    
    * Fix undefined referenc to boost system
    
    * Change 2 indents to 4

--- routing/src/NetworkConnector/NetworkConnector.cpp
+++ routing/src/NetworkConnector/NetworkConnector.cpp
@@ -7,24 +7,54 @@
 
 using namespace m2::routing;
 
+NetworkConnector::NetworkConnector(
+    int port,
+    io_service& io_service,
+    std::function<void(vector<char>)> on_message_callback)
+    : socket(io_service, udp::endpoint(udp::v4(), port))
+    , on_message_callback(on_message_callback)
+{}
+
+
+void NetworkConnector::startAccept()
+{
+    udp::endpoint sender_endpoint;
+    socket.async_receive_from(
+        boost::asio::buffer(buffer),
+        sender_endpoint,
+        [this](const boost::system::error_code& e, size_t size){
+            if (!e) {
+                vector<char> received_buff(buffer.cbegin(), buffer.cend());
+                this->on_message_callback(received_buff);
+                this->startAccept();
+                return;
+            } else {
+                //залогировать ошибку
+            }
+        });
+}
+
+
 std::string NetworkConnector::getMyIpAddress()
 {
-    boost::asio::io_service io_service;
 
-    boost::asio::ip::tcp::resolver resolver(io_service);
+    boost::asio::ip::tcp::resolver resolver(socket.get_io_service());
     boost::asio::ip::tcp::resolver::query query(boost::asio::ip::host_name(), "");
     boost::asio::ip::tcp::resolver::iterator it = resolver.resolve(query);
     boost::asio::ip::tcp::endpoint endpoint = *it;
     return endpoint.address().to_string();
 }
 
-void NetworkConnector::sendMessage(string ip, int port, vector<char> buffer)
+void NetworkConnector::sendMessage(string ip, int port, vector<char> buff)
 {
-    boost::asio::io_service io_service;
-    boost::asio::ip::udp::endpoint endpoint(boost::asio::ip::address::from_string(ip), port);
-    boost::asio::ip::udp::socket socket(io_service, boost::asio::ip::udp::endpoint(boost::asio::ip::udp::v4(), 0));
-    socket.send_to(boost::asio::buffer(buffer, buffer.size()), endpoint);
-    socket.close();
+    udp::endpoint target(address::from_string(ip), port);
+    socket.async_send_to(
+        boost::asio::buffer(buff),
+        target,
+        [](const boost::system::error_code& e, size_t bytes_transered) {
+            if (e) {
+            //заллогировать
+            } else {
+            }
+        });
 }
-
-NetworkConnector::NetworkConnector() {}
